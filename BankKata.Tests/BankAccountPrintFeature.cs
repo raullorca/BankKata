66f466130d6b21ce61e9301f7c65a5fd0db8eb6e
Ruben Aguilar <ruben.aguilar.becerra@gmail.com>
2017-10-23 23:18:44 +0200

    Added changes from Kata

--- BankKata.Tests/BankAccountPrintFeature.cs
+++ BankKata.Tests/BankAccountPrintFeature.cs
@@ -11,13 +11,21 @@ namespace BankKata.Tests
 {
     public class BankAccountPrintFeature
     {
+        private Mock<IWrite> writer;
+        private Mock<IDate> date;
+        private IBankAccount bankAccount;
+
+        public BankAccountPrintFeature()
+        {
+            writer = new Mock<IWrite>();
+            date = new Mock<IDate>();
+            bankAccount = new BankAccount(writer.Object, date.Object);
+
+        }
+
         [Fact]
         public void PrintAccountStatment()
         {
-            Mock<IWrite> write = new Mock<IWrite>();
-
-            IBankAccount bankAccount = new BankAccount();
-
             bankAccount.Deposit(1000);
             bankAccount.Withdraw(100); // or -100
             bankAccount.Deposit(500);
@@ -25,14 +33,54 @@ namespace BankKata.Tests
 
             var values = new List<string>();
 
-            write
+            writer
                 .Setup(x => x.Print(It.IsAny<string>()))
                 .Callback<string>((text) => values.Add(text));
 
-            write.Verify(x => x.Print("DATE       | AMOUNT  | BALANCE"));
-            write.Verify(x => x.Print("10/04/2014 | 500.00  | 1400.00"));
-            write.Verify(x => x.Print("02/04/2014 | -100.00 | 900.00"));
-            write.Verify(x => x.Print("01/04/2014 | 1000.00 | 1000.00"));
+            writer.Verify(x => x.Print("DATE       | AMOUNT  | BALANCE"));
+            writer.Verify(x => x.Print("10/04/2014 | 500.00  | 1400.00"));
+            writer.Verify(x => x.Print("02/04/2014 | -100.00 | 900.00"));
+            writer.Verify(x => x.Print("01/04/2014 | 1000.00 | 1000.00"));
+        }
+
+        [Fact]
+        public void ShouldPrintAnEmptyBalanceWhenThereAreNotMovements()
+        {
+            bankAccount.PrintStatement();
+
+            writer.Verify(x => x.Print("DATE       | AMOUNT  | BALANCE"));
+        }
+        [Fact]
+        public void ShouldByAddDepositShowNewBalancePositive()
+        {
+            date.Setup(d => d.Date).Returns(new DateTime(2014, 4, 1));
+            bankAccount.Deposit(1000);
+            bankAccount.PrintStatement();
+
+            writer.Verify(x => x.Print("DATE       | AMOUNT  | BALANCE"));
+            writer.Verify(x => x.Print("01/04/2014 | 1000,00 | 1000,00"));
+        }
+
+        [Fact]
+        public void ShouldByAddWithdrawShowNewBalanceNegative()
+        {
+            date.Setup(d => d.Date).Returns(new DateTime(2014, 4, 2));
+            bankAccount.Withdraw(-100);
+            bankAccount.PrintStatement();
+
+            writer.Verify(x => x.Print("DATE       | AMOUNT  | BALANCE"));
+            writer.Verify(x => x.Print("02/04/2014 | -100,00 | -100,00"));
+        }
+
+        [Fact]
+        public void ShouldByAddWithdrawShowNewBalanceNegative_2()
+        {
+            date.Setup(d => d.Date).Returns(new DateTime(2014, 4, 2));
+            bankAccount.Withdraw(100);
+            bankAccount.PrintStatement();
+
+            writer.Verify(x => x.Print("DATE       | AMOUNT  | BALANCE"));
+            writer.Verify(x => x.Print("02/04/2014 | -100,00 | -100,00"));
         }
     }
 }
---
+++ BankKata/Movement.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace BankKata
+{
+    public class Movement
+    {
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
